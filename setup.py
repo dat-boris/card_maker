ad902d0c9c368b8bf2df61440abe519b4d219a3a
Boris Lau <boris@techie.im>
2020-10-29 18:00:44 -0700

    Added requiremnts

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 import setuptools
 
+REQUIREMENTS = [
+    "imgkit",
+    "pageshot",
+    "pillow",
+    "pyyaml",
+    "jinja2",
+    "google-api-python-client",
+    "google-auth-httplib2",
+    "google-auth-oauthlib",
+]
+
 setuptools.setup(
     name="playtest_cards",
     version="0.0.1",
@@ -7,6 +18,7 @@ setuptools.setup(
     description="A python package for making cards quickly",
     url="https://github.com/dat-boris/card_maker",
     packages=["playtest_cards"],
+    install_requires=REQUIREMENTS,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
